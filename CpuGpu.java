fd70ef08e5a2527fd3f4452a1dc17d693df5b3b9
Alkın Alkan <83280716+alkinalkan@users.noreply.github.com>
2023-05-21 13:48:54 +0300

    Add files via upload

---
+++ CpuGpu.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+
+public class CpuGpu {
+
+  public static void main(String[] args) throws I0Exception {
+    try {
+      ProcessBuilder launcher = new ProcessBuilder();
+      Map < String,String > environment = launcher.environment();
+      launcher.redirectErrorStream(true);
+      launcher.directory(new File("destinationOfWindowsPowerShellExe"));
+      launcher.command("powershell.exe", "destinationOfPowershellFile");
+      Process p = launcher.start() : // And launch a new process
+      BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+      String line;
+      while ((line = stdInput.readline()) != null) {
+        System.out.println(line);
+      }
+    }
+    catch(Exception e) {
+      e.printStackTrace;
+    }
+  }
+}
